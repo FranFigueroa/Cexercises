041b7012af941b6c03dd3a7cf62100ad42357317
FranFigueroa <francofigueroa321@gmail.com>
2023-03-26 23:54:00 -0300

    solucion

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,34 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 int tarea(int);
-int main(void){
-    pid_t pid, pidTater;
-    int status1 = 0, status2 = 0, status3 = 0; /*(PID) identificador de proceso*/
-    pid=fork();
 
-    if (pid == 0){
+int main(void) {
+    pid_t pid, pidTer;
+    int status1 = 0, status2 = 0, status3 = 0;
+    pid = fork();
+    if (pid == 0) {
         pid = fork();
-        if (pid>0){  /*la funcion pid ==0 si se trata del proceso hijo. Por lo que verificamos que se trate de este*/
+        if (pid > 0) {
             pid = fork();
-            if (pid == 0 )
-                exit(tarea(gptpid()));
-                else{
-                    pidTer = ----------------------------------;
-                    printf("termino %d informa %d\n",pidTer,status1/256);
-                    pidTer = ----------------------------------;
-                    printf("termino %d informa %d\n",pidTer,status2/256);
-                    exit(status1 && status2);
-                }
-        }  else exit(tarea(getpid()));
- } else {
- if (pid > 0 ) {
- pidTer = --------------------------------;
- printf("termino %d informa %d\n",pidTer,status3/256);
- }
- }
- exit(pid);
-}
-int tarea(int num) {
- printf("tarea %d \n",num);
- return num / 2000 ;
+            if (pid == 0) {
+                exit(tarea(getpid()));
+            } else {
+                pidTer = pid;
+                waitpid(pidTer, &status1, 0);
+                printf("termino %d informa %d\n", pidTer, status1/256);
+                exit(status1);
+            }
+        } else {
+            exit(tarea(getpid()));
+        }
+    } else {
+        if (pid > 0) {
+            pidTer = pid;
+            waitpid(pidTer, &status2, 0);
+            printf("termino %d informa %d\n", pidTer, status2/256);
+        }
+    }
+    exit(pid);
 }
 
-    
+int tarea(int num) {
+    printf("tarea %d \n", num);
+    return num / 2000;
+}
